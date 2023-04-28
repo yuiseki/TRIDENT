d775a06b57d992439d72542bcdc20df3814f7061
yuiseki <yuiseki@gmail.com>
2023-04-28 09:08:32 +0900

    update github repositories

--- scripts/github.com/fetch_and_store.ts
+++ scripts/github.com/fetch_and_store.ts
@@ -7,13 +7,26 @@ import fs from "node:fs/promises";
 
 dotenv.config();
 
+const sleep = (msec: number) =>
+  new Promise((resolve) => setTimeout(resolve, msec));
+
 const urlsFile = await fs.readFile("public/github.com/urls.txt", "utf-8");
 const urls = urlsFile.split("\n");
 console.info(urls.length);
 
 const docs = [];
-
 const splitter = new MarkdownTextSplitter();
+
+// GitHub API共通認証ヘッダー
+const githubHeaders = {
+  Authorization: `Bearer ${process.env.GITHUB_APP_USER_TOKEN}`,
+};
+
+// GitHub APIのRate Limit緩和リクエスト
+const rateLimitRes = await fetch("https://api.github.com/meta", {
+  headers: githubHeaders,
+});
+console.log(rateLimitRes.headers);
 
 for await (const url of urls) {
   console.info(url);
@@ -21,47 +34,59 @@ for await (const url of urls) {
     continue;
   }
   try {
-    const metadataUrl =
-      "https://api.github.com/repos/" + url.replace("https://github.com/", "");
-    const metadataRes = await fetch(metadataUrl);
-    const metadataJson = await metadataRes.json();
-
     const metadata = {
-      name: metadataJson.name,
-      full_name: metadataJson.full_name,
-      author: metadataJson.owner.login,
-      authorUrl: metadataJson.owner.html_url,
-      title: metadataJson.full_name + "; " + metadataJson.description,
-      license: metadataJson.license.name,
+      name: "",
+      fullName: "",
+      authorUrl: "",
+      author: "",
+      title: "",
+      licence: "",
     };
+    try {
+      const metadataUrl =
+        "https://api.github.com/repos/" +
+        url.replace("https://github.com/", "");
+      const metadataRes = await fetch(metadataUrl, { headers: githubHeaders });
+      const metadataJson = await metadataRes.json();
+      console.log(metadataRes.headers);
+      console.log(metadataJson);
+      metadata.title = metadataJson.full_name + "; " + metadataJson.description;
+      metadata.fullName = metadataJson.full_name;
+      if (metadataJson.licence && metadataJson.licence.name) {
+        metadata.licence = metadataJson.licence.name;
+      }
+      try {
+        metadata.name = metadataJson.name;
+      } catch (error) {
+        console.log(metadataJson);
+      }
+
+      try {
+        metadata.authorUrl = metadataJson.owner.html_url;
+        metadata.author = metadataJson.owner.login;
+      } catch (error) {
+        console.log(metadataJson);
+      }
+    } catch (error) {
+      console.error(error);
+    }
 
     let readMeUrl = "";
 
-    const ghPagesBranchReadMeUrl = url + "/raw/gh-pages/README.md";
-    const ghPagesBranchReadMeHeadRes = await fetch(ghPagesBranchReadMeUrl, {
-      method: "HEAD",
-      redirect: "follow",
-    });
-    if (ghPagesBranchReadMeHeadRes.ok) {
-      readMeUrl = ghPagesBranchReadMeUrl;
-    }
-
-    const masterBranchReadMeUrl = url + "/raw/master/README.md";
-    const masterBranchReadMeHeadRes = await fetch(masterBranchReadMeUrl, {
-      method: "HEAD",
-      redirect: "follow",
-    });
-    if (masterBranchReadMeHeadRes.ok) {
-      readMeUrl = masterBranchReadMeUrl;
-    }
-
-    const mainBranchReadMeUrl = url + "/raw/main/README.md";
-    const mainBranchReadMeHeadRes = await fetch(mainBranchReadMeUrl, {
-      method: "HEAD",
-      redirect: "follow",
-    });
-    if (mainBranchReadMeHeadRes.ok) {
-      readMeUrl = mainBranchReadMeUrl;
+    const readMeCandidates = [
+      "/raw/gh-pages/README.md",
+      "/raw/master/README.md",
+      "/raw/main/README.md",
+    ];
+    for await (const readMeCandidate of readMeCandidates) {
+      const readMeCandidateUrl = url + readMeCandidate;
+      const readMeCandidateRes = await fetch(readMeCandidateUrl, {
+        method: "HEAD",
+        redirect: "follow",
+      });
+      if (readMeCandidateRes.ok) {
+        readMeUrl = readMeCandidateUrl;
+      }
     }
 
     console.log(readMeUrl);
@@ -77,6 +102,7 @@ for await (const url of urls) {
   } catch (error) {
     console.error(error);
   }
+  await sleep(1000);
 }
 
 console.log(docs.length);
