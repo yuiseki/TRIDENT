f08bc91acaa9c9bbeca0faa5f075aca0e382b3d4
yuiseki <yuiseki@gmail.com>
2023-05-04 16:24:40 +0900

    remove value from button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -281,7 +281,6 @@ export default function Home() {
               }}
             />
             <button
-              value=""
               disabled={
                 responding ||
                 lazyInserting ||
