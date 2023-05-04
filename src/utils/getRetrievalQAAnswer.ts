406352161853232162dbb52135515d682fd02fe8
yuiseki <yuiseki@gmail.com>
2023-05-04 13:19:12 +0900

    add comment

--- src/utils/getRetrievalQAAnswer.ts
+++ src/utils/getRetrievalQAAnswer.ts
@@ -12,6 +12,8 @@ export const getRetrievalQAAnswer = async (query: string) => {
     environment: process.env.PINECONE_ENVIRONMENT || "",
   });
   const pineconeIndex = client.Index(process.env.PINECONE_INDEX || "");
+
+  // initialize vector store
   const vectorStore = await PineconeStore.fromExistingIndex(
     new OpenAIEmbeddings(),
     { pineconeIndex }
