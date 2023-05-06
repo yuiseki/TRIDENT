ddcc3456441adcc381c4d0ddec4be03a394b4de4
yuiseki <yuiseki@gmail.com>
2023-05-06 14:15:50 +0900

    vector store for disasters

--- scripts/api.reliefweb.int/02_store.ts
+++ scripts/api.reliefweb.int/02_store.ts
@@ -2,3 +2,28 @@ import fs from "node:fs/promises";
 import * as dotenv from "dotenv";
 import { OpenAI } from "langchain/llms/openai";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { HNSWLib } from "langchain/vectorstores";
+import { Document } from "langchain/document";
+
+dotenv.config();
+
+const latestSummaryFilePath =
+  "public/api.reliefweb.int/disasters/summaries/latest_summary.json";
+const latestSummaryFile = await fs.readFile(latestSummaryFilePath, "utf-8");
+const latestSummaryJson = JSON.parse(latestSummaryFile);
+
+const docs: Document[] = [];
+for await (const disaster of latestSummaryJson.disasters) {
+  const doc = new Document({
+    pageContent: disaster.summary,
+    metadata: disaster.metadata,
+  });
+  docs.push(doc);
+}
+
+console.log(docs[0]);
+
+const vectorStoreSaveDir =
+  "public/api.reliefweb.int/disasters/summaries/vector_stores/";
+const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+await vectorStore.save(vectorStoreSaveDir);
---
+++ scripts/api.reliefweb.int/99_test.ts
@@ -0,0 +1,18 @@
+import * as dotenv from "dotenv";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { HNSWLib } from "langchain/vectorstores";
+
+dotenv.config();
+
+const vectorStoreSaveDir =
+  "public/api.reliefweb.int/disasters/summaries/vector_stores/";
+const loadedVectorStore = await HNSWLib.load(
+  vectorStoreSaveDir,
+  new OpenAIEmbeddings()
+);
+
+const result = await loadedVectorStore.similaritySearch(
+  "What is latest situation in Sudan?",
+  4
+);
+console.log(result);
