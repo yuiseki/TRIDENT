89369c3ad569ffa0cffc0321e42b3d0f25d7c34a
yuiseki <yuiseki@gmail.com>
2023-05-10 08:45:49 +0900

    tweak

--- src/utils/langchain/tools/summarization/index.ts
+++ src/utils/langchain/tools/summarization/index.ts
@@ -3,14 +3,15 @@ import { BaseLanguageModel } from "langchain/dist/base_language";
 import { PromptTemplate } from "langchain/prompts";
 import { ChainTool } from "langchain/tools";
 
-const summarizationPrompt = PromptTemplate.fromTemplate(
-  "You are an AI who always concisely summarize given text as short as possible. Summarise the following text in a nutshell: {text}"
-);
-
 export const loadSummarizationChainTool = async (llm: BaseLanguageModel) => {
   return new ChainTool({
     name: "text-summarization",
-    chain: new LLMChain({ llm: llm, prompt: summarizationPrompt }),
+    chain: new LLMChain({
+      llm: llm,
+      prompt: PromptTemplate.fromTemplate(
+        "You are an AI who always concisely summarize given text as short as possible. Summarise the following text in a nutshell: {text}"
+      ),
+    }),
     description:
       "useful for when you need to summarize a text. Input: a text. Output: a summary of text.",
   });
--- test/questions.ts
+++ test/questions.ts
@@ -1,8 +1,8 @@
 export const questions = [
+  "What is happening in Sudan right now?",
   "What is the United Nations in a nutshell?",
   "When and where was the United Nations Charter signed?",
   "What is the latest situation in Ukraine?",
   "What is the latest situation in Sudan?",
   "What is happening in Ukraine right now?",
-  "What is happening in Sudan right now?",
 ];
