9cf87574536093c74384348f6752711613a5cf94
yuiseki <yuiseki@gmail.com>
2023-06-02 20:34:02 +0900

    refine

--- src/app/api/inner/route.ts
+++ src/app/api/inner/route.ts
@@ -6,34 +6,38 @@ export async function POST(request: Request) {
   const res = await request.json();
   const pastMessagesJsonString = res.pastMessages;
 
-  let chatHistory: string[] = [];
+  let chatHistory: Array<string | null> = [];
+  let chatHistoryLines = "";
   if (pastMessagesJsonString && pastMessagesJsonString !== "undefined") {
     const pastMessages: {
       messages: Array<{ type: string; data: { content: string } }>;
     } = JSON.parse(pastMessagesJsonString);
-    chatHistory = pastMessages.messages.map((message, idx) => {
-      if (message.data.content) {
-        if (idx === 0 || idx % 2 === 0) {
-          return `Human: ${message.data.content}`;
+    chatHistory = pastMessages.messages
+      .map((message, idx) => {
+        if (message.data.content) {
+          if (idx === 0 || idx % 2 === 0) {
+            return `Human: ${message.data.content}`;
+          } else {
+            return null;
+            //return `AI: ${message.data.content}`;
+          }
         } else {
-          return "";
-          //return `AI: ${message.data.content}`;
+          return null;
         }
-      } else {
-        return "";
-      }
-    });
+      })
+      .filter((v) => v);
+    chatHistoryLines = chatHistory.join("\n").replace("\n\n", "\n");
   }
 
   console.log("----- ----- -----");
   console.log("----- inner -----");
   console.log("----- ----- -----");
-  console.log(chatHistory.join("\n").replace("\n\n", "\n"));
+  console.log(chatHistoryLines);
 
   const model = new OpenAI({ temperature: 0 });
   const chain = loadTridentInnerChain({ llm: model });
   const result = await chain.call({
-    chat_history: chatHistory.join("\n").replace("\n\n", "\n"),
+    chat_history: chatHistoryLines,
   });
   console.log(result.text);
   console.log("");
