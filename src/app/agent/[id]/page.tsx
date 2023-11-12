a13f43ff1a3e039d48d38f41841b11b7c4a8ad41
yuiseki <yuiseki@gmail.com>
2023-11-12 19:58:22 +0900

    OpenAI破産を回避

--- src/app/agent/[id]/page.tsx
+++ src/app/agent/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import useSWR from "swr";
 import { jsonFetcher } from "@/utils/jsonFetcher";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import React from "react";
 import { nextPostJson, nextPostJsonWithCache } from "@/utils/nextPostJson";
 import { MapProvider, MapRef } from "react-map-gl";
@@ -14,13 +14,15 @@ import osmtogeojson from "osmtogeojson";
 import { getOverpassResponseJsonWithCache } from "@/utils/getOverpassResponse";
 import { BaseMap } from "@/components/BaseMap";
 import { GeoJsonToMarkers } from "@/components/GeoJsonToMarkers";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
 
 const formatDate = (date: Date) => {
   return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
 };
 
 type ConcernEvent = {
-  link: string;
+  url: string;
   description: string;
   title: string;
   pubDate: string;
@@ -30,24 +32,55 @@ type ConcernEvent = {
   requestToDisplayMaps: string[];
 };
 
-const NewsWithMap: React.FC<{ concern: ConcernEvent }> = ({ concern }) => {
+export default function Page() {
+  // Next.js routerからidを得る
+  const pathname = usePathname();
+  const id = pathname.split("/").pop();
   const mapRef = useRef<MapRef | null>(null);
   const [geojsonWithStyleList, setGeojsonWithStyleList] = useState<
     Array<{ id: string; style: TridentMapsStyle; geojson: FeatureCollection }>
   >([]);
   const [mapTitle, setMapTitle] = useState<string | undefined>(undefined);
+  const [concern, setConcern] = useState<ConcernEvent | undefined>(undefined);
+  const [mapping, setMapping] = useState(true);
 
   // base maps
   const [mapStyleJsonUrl, setMapStyleJsonUrl] = useLocalStorage<string>(
     "trident-selected-map-style-json-url",
     "/map_styles/fiord-color-gl-style/style.json"
   );
+
+  const { data, error } = useSWR<Array<ConcernEvent>>(
+    "/data/www3.nhk.or.jp/concerns/latest_concerns.json",
+    jsonFetcher
+  );
+
   const onSelectMapStyleJsonUrl = useCallback(
     (e: React.ChangeEvent<HTMLSelectElement>) => {
       setMapStyleJsonUrl(e.target.value);
     },
     [setMapStyleJsonUrl]
   );
+
+  useEffect(() => {
+    // dataがない場合は何もしない
+    if (!data) {
+      return;
+    }
+    // idがない場合は何もしない
+    if (!id) {
+      return;
+    }
+    // idに該当するconcernを探し、newConcernに格納する
+    const newConcern = data.find((concern) =>
+      concern.url.includes(id as string)
+    );
+    if (!newConcern) {
+      return;
+    }
+    // concernが変わったら、stateを更新する
+    setConcern(newConcern);
+  }, [data, id]);
 
   useEffect(() => {
     setTimeout(() => {
@@ -75,35 +108,39 @@ const NewsWithMap: React.FC<{ concern: ConcernEvent }> = ({ concern }) => {
     }, 500);
   }, [geojsonWithStyleList]);
 
-  const [responding, setResponding] = useState(false);
-  const [mapping, setMapping] = useState(false);
-
-  const currentDate = new Date(concern.currentDate);
-  const whenHappens = `${formatDate(currentDate)}、`;
-  const whereHappens = `${concern.where}。`;
-  const whereAndWhenHappens = whenHappens + whereHappens;
-  const whatHappens = concern.whatHappenings;
-  const displayMaps = concern.requestToDisplayMaps;
+  const whereAndWhenHappens = useMemo(() => {
+    // concernがない場合は何もしない
+    if (!concern) {
+      return "";
+    }
+    const currentDate = new Date(concern.currentDate);
+    const whenHappens = `${formatDate(currentDate)}、`;
+    const whereHappens = `${concern.where}。`;
+    return whenHappens + whereHappens;
+  }, [concern]);
 
   useEffect(() => {
     const f = async () => {
-      const pastMessages = displayMaps.map((requestToDisplayMap) => {
-        const cleanedRequestToDisplayMap = requestToDisplayMap
-          .replaceAll("北東部", "")
-          .replaceAll("東部", "")
-          .replaceAll("西部", "");
-        return {
-          type: "constructor",
-          id: ["langchain", "schema", "HumanMessage"],
-          kwargs: {
-            content: cleanedRequestToDisplayMap,
-          },
-        } as {
-          type: string;
-          id: string[];
-          kwargs: { content: string };
-        };
-      });
+      const pastMessages = concern?.requestToDisplayMaps.map(
+        (requestToDisplayMap) => {
+          const cleanedRequestToDisplayMap = requestToDisplayMap
+            .replaceAll("北東部", "")
+            .replaceAll("東部", "")
+            .replaceAll("西部", "")
+            .replaceAll("山岳地帯の", "");
+          return {
+            type: "constructor",
+            id: ["langchain", "schema", "HumanMessage"],
+            kwargs: {
+              content: cleanedRequestToDisplayMap,
+            },
+          } as {
+            type: string;
+            id: string[];
+            kwargs: { content: string };
+          };
+        }
+      );
       console.log("pastMessages", pastMessages);
 
       // call inner layer
@@ -117,7 +154,6 @@ const NewsWithMap: React.FC<{ concern: ConcernEvent }> = ({ concern }) => {
         return;
       }
       if (innerResJson.inner.toLowerCase().includes("no map")) {
-        setResponding(false);
         setMapping(false);
         return;
       }
@@ -217,13 +253,19 @@ const NewsWithMap: React.FC<{ concern: ConcernEvent }> = ({ concern }) => {
       });
     };
     f();
-  }, [displayMaps]);
+  }, [concern]);
+
+  // concernがない場合は何もしない
+  if (!concern) {
+    return <div></div>;
+  }
 
   return (
     <div
       style={{
         width: "100vw",
         height: "100vh",
+        backgroundColor: "#fff",
       }}
     >
       <div
@@ -232,8 +274,9 @@ const NewsWithMap: React.FC<{ concern: ConcernEvent }> = ({ concern }) => {
           padding: "1rem",
         }}
       >
+        <Link href="/agent">&lt; /agent</Link>
         <h2 style={{ fontSize: "3em" }}>{whereAndWhenHappens}</h2>
-        <h3 style={{ fontSize: "1.8em" }}>{whatHappens}</h3>
+        <h3 style={{ fontSize: "1.8em" }}>{concern.whatHappenings}</h3>
         <p>
           <ul
             style={{
@@ -241,9 +284,10 @@ const NewsWithMap: React.FC<{ concern: ConcernEvent }> = ({ concern }) => {
               marginLeft: "1.5rem",
             }}
           >
-            {displayMaps.map((req, idx) => {
-              return <li key={`${concern.link}-req-${idx}`}>{req}</li>;
+            {concern.requestToDisplayMaps.map((req, idx) => {
+              return <li key={`${concern.url}-req-${idx}`}>{req}</li>;
             })}
+            {mapping && <li>loading...</li>}
           </ul>
         </p>
       </div>
@@ -263,7 +307,7 @@ const NewsWithMap: React.FC<{ concern: ConcernEvent }> = ({ concern }) => {
                 console.log(geojsonWithStyle);
                 return (
                   <GeoJsonToMarkers
-                    key={`${concern.link}-geojson-${idx}`}
+                    key={`${concern.url}-geojson-${idx}`}
                     geojson={geojsonWithStyle.geojson}
                     style={geojsonWithStyle.style}
                   />
@@ -291,130 +335,6 @@ const NewsWithMap: React.FC<{ concern: ConcernEvent }> = ({ concern }) => {
           ↓
         </div>
       </div>
-    </div>
-  );
-};
-
-export default function Page() {
-  const [sortedConcerns, setSortedConcerns] = useState<
-    Array<ConcernEvent> | undefined
-  >(undefined);
-
-  const { data, error } = useSWR<Array<ConcernEvent>>(
-    "/data/www3.nhk.or.jp/concerns/latest_concerns.json",
-    jsonFetcher
-  );
-
-  useEffect(() => {
-    if (!data) {
-      return;
-    }
-
-    const newSortedConcerns = data
-      .filter((v) => v)
-      .filter((concern) => {
-        if (
-          concern.currentDate === null ||
-          concern.currentDate === "Unknown" ||
-          concern.currentDate === "unknown" ||
-          concern.where === "Unknown" ||
-          concern.where === "unknown" ||
-          concern.requestToDisplayMaps.length === 0
-        ) {
-          return false;
-        }
-        return true;
-      })
-      .filter((concern) => {
-        if (
-          concern.whatHappenings
-            .map(
-              (happenings) =>
-                happenings.includes("目指す") || happenings.includes("関係各国")
-            )
-            .includes(true) ||
-          concern.requestToDisplayMaps
-            .map((req) => req.includes("近海"))
-            .includes(true)
-        ) {
-          return false;
-        } else {
-          return true;
-        }
-      })
-      .sort((a, b) => {
-        return (
-          new Date(b.currentDate).getTime() - new Date(a.currentDate).getTime()
-        );
-      });
-    setSortedConcerns(newSortedConcerns);
-  }, [data]);
-
-  const getConcerns = useCallback(
-    (start: Date, end: Date) => {
-      if (!sortedConcerns) {
-        return;
-      }
-      console.log("sortedConcerns", sortedConcerns.length);
-      const filteredConcerns = sortedConcerns.filter((event) => {
-        return (
-          start.getTime() <= new Date(event.currentDate).getTime() &&
-          new Date(event.currentDate).getTime() <= end.getTime()
-        );
-      });
-      console.log("filteredConcerns", filteredConcerns.length);
-      return filteredConcerns;
-    },
-    [sortedConcerns]
-  );
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "#fff",
-      }}
-    >
-      {Array.from({ length: 12 }, (_, i) => i + 1).map((weekIndex) => {
-        console.log("week: ", weekIndex);
-        const now = new Date();
-        // 今日が何曜日かを取得する
-        const dayOfWeek = now.getDay();
-        // 日曜日を基準として、startDateとendDateは、1週間ごとに設定する
-        // ただし、今日が日曜日の場合は、startDateとendDateは、今日から1週間前とする
-        const startDate = new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          now.getDate() - 7 * (weekIndex - 1) - dayOfWeek
-        );
-
-        const endDate = new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          now.getDate() - 7 * (weekIndex - 1) - dayOfWeek + 6
-        );
-
-        const weeklyConcerns = getConcerns(startDate, endDate);
-        if (!weeklyConcerns) return null;
-        if (weeklyConcerns.length === 0) return null;
-        console.log("weeklyConcerns", weeklyConcerns.length);
-        return (
-          <>
-            {weeklyConcerns &&
-              weeklyConcerns.slice(0, 2).map((concern) => {
-                console.log(concern);
-                console.log(concern.link);
-                return (
-                  <NewsWithMap
-                    key={`${concern.link}-${weekIndex}-0`}
-                    concern={concern}
-                  />
-                );
-              })}
-          </>
-        );
-      })}
     </div>
   );
 }
--- src/app/agent/page.tsx
+++ src/app/agent/page.tsx
@@ -1,27 +1,157 @@
 "use client";
 
-import { BaseMap } from "@/components/BaseMap";
-import { useRef } from "react";
-import { MapProvider, MapRef } from "react-map-gl";
+import useSWR from "swr";
+import { jsonFetcher } from "@/utils/jsonFetcher";
+import { useCallback, useEffect, useRef, useState } from "react";
+import React from "react";
+import Link from "next/link";
+
+const formatDate = (date: Date) => {
+  return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
+};
+
+type ConcernEvent = {
+  url: string;
+  description: string;
+  title: string;
+  pubDate: string;
+  currentDate: string;
+  where: string;
+  whatHappenings: string[];
+  requestToDisplayMaps: string[];
+};
 
 export default function Page() {
+  const [sortedConcerns, setSortedConcerns] = useState<
+    Array<ConcernEvent> | undefined
+  >(undefined);
+
+  const { data, error } = useSWR<Array<ConcernEvent>>(
+    "/data/www3.nhk.or.jp/concerns/latest_concerns.json",
+    jsonFetcher
+  );
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+
+    const newSortedConcerns = data
+      .filter((v) => v)
+      .filter((concern) => {
+        if (
+          concern.currentDate === null ||
+          concern.currentDate === "Unknown" ||
+          concern.currentDate === "unknown" ||
+          concern.where === "Unknown" ||
+          concern.where === "unknown" ||
+          concern.requestToDisplayMaps.length === 0
+        ) {
+          return false;
+        }
+        return true;
+      })
+      .filter((concern) => {
+        if (
+          concern.whatHappenings
+            .map(
+              (happenings) =>
+                happenings.includes("目指す") || happenings.includes("関係各国")
+            )
+            .includes(true) ||
+          concern.requestToDisplayMaps
+            .map((req) => req.includes("近海"))
+            .includes(true)
+        ) {
+          return false;
+        } else {
+          return true;
+        }
+      })
+      .sort((a, b) => {
+        return (
+          new Date(b.currentDate).getTime() - new Date(a.currentDate).getTime()
+        );
+      });
+    setSortedConcerns(newSortedConcerns);
+  }, [data]);
+
+  const getConcerns = useCallback(
+    (start: Date, end: Date) => {
+      if (!sortedConcerns) {
+        return;
+      }
+      console.log("sortedConcerns", sortedConcerns.length);
+      const filteredConcerns = sortedConcerns.filter((event) => {
+        return (
+          start.getTime() <= new Date(event.currentDate).getTime() &&
+          new Date(event.currentDate).getTime() <= end.getTime()
+        );
+      });
+      console.log("filteredConcerns", filteredConcerns.length);
+      return filteredConcerns;
+    },
+    [sortedConcerns]
+  );
+
   return (
-    <div className="tridentAgentReportListWrap">
-      <div className="tridentAgentReportList">
-        <h3>TRIDENT Agent</h3>
-        <ul>
-          <li>
-            <a href="/agent/eq-2023-000166-mar">
-              2023年9月 モロッコ地震 レポート
-            </a>
-          </li>
-          <li>
-            <a href="/agent/fl-2023-000168-lby">
-              2023年9月 リビア洪水 レポート
-            </a>
-          </li>
-        </ul>
-      </div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#fff",
+        padding: "1.5rem",
+      }}
+    >
+      <ul>
+        {Array.from({ length: 12 }, (_, i) => i + 1).map((weekIndex) => {
+          const now = new Date();
+          // 今日が何曜日かを取得する
+          const dayOfWeek = now.getDay();
+          // 日曜日を基準として、startDateとendDateは、1週間ごとに設定する
+          // ただし、今日が日曜日の場合は、startDateとendDateは、今日から1週間前とする
+          const startDate = new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate() - 7 * (weekIndex - 1) - dayOfWeek
+          );
+
+          const endDate = new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate() - 7 * (weekIndex - 1) - dayOfWeek + 6
+          );
+
+          const weeklyConcerns = getConcerns(startDate, endDate);
+          if (!weeklyConcerns) return null;
+          if (weeklyConcerns.length === 0) return null;
+          return (
+            <>
+              {weeklyConcerns &&
+                weeklyConcerns.map((concern) => {
+                  const concernUrlArray = concern.url.split("/");
+                  console.log(
+                    "permalink",
+                    concernUrlArray[concernUrlArray.length - 1]
+                  );
+                  return (
+                    <li key={concern.url} style={{ padding: "0.5rem 0" }}>
+                      <Link
+                        href={`/agent/${concernUrlArray[
+                          concernUrlArray.length - 1
+                        ].replaceAll(".html", "")}`}
+                      >
+                        {concern.title}
+                      </Link>
+                      <br />
+                      {concern.requestToDisplayMaps}
+                    </li>
+                  );
+                })}
+            </>
+          );
+        })}
+      </ul>
     </div>
   );
 }
