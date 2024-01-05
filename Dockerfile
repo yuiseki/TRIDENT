891c514f8ae293db0f3149b7f206f4ac5e737ba6
yuiseki <yuiseki@gmail.com>
2024-01-05 14:12:11 +0900

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM node:21-bullseye-slim
 
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    python3 \
+    python3-pip \
+    python3-dev \
+    python3-setuptools \
+    python3-wheel \
+    && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /app
 COPY . /app
 
--- src/utils/langchain/llms/llama_cpp_server.ts
+++ src/utils/langchain/llms/llama_cpp_server.ts
@@ -1,64 +1,62 @@
-import { LLM, BaseLLMParams, BaseLLMCallOptions } from "langchain/llms/base";
+import { LLM, BaseLLMParams } from "langchain/llms/base";
 
-type LlamaCppServerCompletionCallParams = {
+export interface LlamaCppServerCompletionInput extends BaseLLMParams {
   temperature?: number;
   n_predict?: number;
   repeat_penalty?: number;
   stop?: string[];
-};
+}
 
-export class LlamaCppServerCompletion extends LLM<
-  BaseLLMCallOptions & LlamaCppServerCompletionCallParams
-> {
-  temperature = 0.8;
-  n_predict = 128;
-  repeat_penalty = 1.1;
-  stop = [];
+export class LlamaCppServerCompletion
+  extends LLM
+  implements LlamaCppServerCompletionInput
+{
+  temperature?: number;
+  n_predict?: number;
+  repeat_penalty?: number;
+  stop?: string[];
 
-  constructor(fields?: BaseLLMParams & LlamaCppServerCompletionCallParams) {
+  constructor(fields?: LlamaCppServerCompletionInput) {
     super(fields ?? {});
-    this.temperature = fields?.temperature ?? this.temperature;
-    this.n_predict = fields?.n_predict ?? this.n_predict;
-    this.repeat_penalty = fields?.repeat_penalty ?? this.repeat_penalty;
-  }
 
-  async _call(
-    prompt: string,
-    options?: this["ParsedCallOptions"]
-  ): Promise<string> {
-    return this.caller.call(async () => {
-      try {
-        const res = await this.callCompletionApi(prompt, options);
-        const resJson = await res.json();
-        return resJson.content;
-      } catch (e) {
-        throw e;
-      }
-    });
+    this.temperature = fields?.temperature ?? 0.0;
+    this.n_predict = fields?.n_predict ?? 64;
+    this.repeat_penalty = fields?.repeat_penalty ?? 1.2;
+    this.stop = fields?.stop ?? [];
   }
 
   _llmType(): string {
     return "llama-cpp-server";
   }
 
-  private async callCompletionApi(
-    input: string,
-    options?: LlamaCppServerCompletionCallParams
-  ) {
-    options = options ?? {};
-    options.temperature = this.temperature;
-    options.n_predict = this.n_predict;
-    options.repeat_penalty = this.repeat_penalty;
-    // options.stop = this.stop;
-    return fetch("http://localhost:8080/completion", {
+  async _call(
+    prompt: string,
+    options: this["ParsedCallOptions"]
+  ): Promise<string> {
+    const res = await this.caller.callWithOptions(
+      { signal: options.signal },
+      this._generateText.bind(this),
+      prompt
+    );
+    return res ?? "";
+  }
+
+  protected async _generateText(input: string) {
+    const body = {
+      prompt: input,
+      temperature: this.temperature,
+      n_predict: this.n_predict,
+      repeat_penalty: this.repeat_penalty,
+      stop: this.stop,
+    };
+    const res = await fetch("http://localhost:8080/completion", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        prompt: input,
-        ...options,
-      }),
+      body: JSON.stringify(body),
     });
+    const json = await res.json();
+    return json.content;
   }
 }
