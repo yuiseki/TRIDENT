a3529bb38ec637fdbf18d5fea23eaf299054a681
yuiseki <yuiseki@gmail.com>
2024-10-24 09:34:34 +0900

    swcMinify: false,

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
   webpack: (config, { isServer, dev }) => {
     config.output.webassemblyModuleFilename =
       isServer && !dev
