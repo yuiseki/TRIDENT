51854fe513b555836496667c9f9fb8281db48af3
yuiseki <yuiseki@gmail.com>
2024-11-03 09:45:49 +0900

    Change type

--- scripts/api.reliefweb.int/analyze_disasters.ts
+++ scripts/api.reliefweb.int/analyze_disasters.ts
@@ -38,99 +38,99 @@ const today = now
 
 type LLMModel = {
   modelName: string;
-  concernDirName: string;
+  modelDirName: string;
 };
 
 const LLMModelAndDirList: LLMModel[] = [
   {
     modelName: "smollm:135m",
-    concernDirName: "concerns/smollm/135m",
+    modelDirName: "smollm/135m",
   },
   {
     modelName: "smollm:360m",
-    concernDirName: "concerns/smollm/360m",
+    modelDirName: "smollm/360m",
   },
   {
     modelName: "smollm:1.7b",
-    concernDirName: "concerns/smollm/1.7b",
+    modelDirName: "smollm/1.7b",
   },
   {
     modelName: "smollm2:135m",
-    concernDirName: "concerns/smollm2/135m",
+    modelDirName: "smollm2/135m",
   },
   {
     modelName: "smollm2:360m",
-    concernDirName: "concerns/smollm2/360m",
+    modelDirName: "smollm2/360m",
   },
   {
     modelName: "smollm2:1.7b",
-    concernDirName: "concerns/smollm2/1.7b",
+    modelDirName: "smollm2/1.7b",
   },
   {
     modelName: "qwen2.5:0.5b",
-    concernDirName: "concerns/qwen2.5/0.5b",
+    modelDirName: "qwen2.5/0.5b",
   },
   {
     modelName: "qwen2.5:1.5b",
-    concernDirName: "concerns/qwen2.5/1.5b",
+    modelDirName: "qwen2.5/1.5b",
   },
   {
     modelName: "qwen2:0.5b",
-    concernDirName: "concerns/qwen2/0.5b",
+    modelDirName: "qwen2/0.5b",
   },
   {
     modelName: "qwen2:1.5b",
-    concernDirName: "concerns/qwen2/1.5b",
+    modelDirName: "qwen2/1.5b",
   },
   {
     modelName: "qwen:0.5b",
-    concernDirName: "concerns/qwen/0.5b",
+    modelDirName: "qwen/0.5b",
   },
   {
     modelName: "qwen:1.8b",
-    concernDirName: "concerns/qwen/1.8b",
+    modelDirName: "qwen/1.8b",
   },
   {
     modelName: "llama3.2:1b",
-    concernDirName: "concerns/llama3.2/1b",
+    modelDirName: "llama3.2/1b",
   },
   {
     modelName: "gemma2:2b",
-    concernDirName: "concerns/gemma2/2b",
+    modelDirName: "gemma2/2b",
   },
   {
     modelName: "gemma:2b-instruct",
-    concernDirName: "concerns/gemma/2b-instruct",
+    modelDirName: "gemma/2b-instruct",
   },
   {
     modelName: "codegemma:2b",
-    concernDirName: "concerns/codegemma/2b",
+    modelDirName: "codegemma/2b",
   },
   {
     modelName: "orca-mini:3b",
-    concernDirName: "concerns/orca-mini/3b",
+    modelDirName: "orca-mini/3b",
   },
   {
     modelName: "phi:2.7b",
-    concernDirName: "concerns/phi/2.7b",
+    modelDirName: "phi/2.7b",
   },
   {
     modelName: "phi3.5:3.8b",
-    concernDirName: "concerns/phi3.5/3.8b",
+    modelDirName: "phi3.5/3.8b",
   },
   {
     modelName: "phi3:3.8b",
-    concernDirName: "concerns/phi3/3.8b",
+    modelDirName: "phi3/3.8b",
   },
 ];
 
 // 解析結果を保存するファイルパス
 const getConcernsTodayJsonFilePath = (llmModel: LLMModel) => {
-  const { concernDirName } = llmModel;
-  const concernDirPath = `public/data/api.reliefweb.int/${concernDirName}`;
+  const { modelDirName } = llmModel;
+  const concernDirPath = `public/data/api.reliefweb.int/concerns/${modelDirName}`;
   // create directory if not exists
   fs.mkdir(concernDirPath, { recursive: true });
-  return `public/data/api.reliefweb.int/${concernDirName}/${today}_concerns.json`;
+  return `public/data/api.reliefweb.int/concerns/${modelDirName}/${today}_concerns.json`;
 };
 
 // 既に解析済みかどうかを確認する
@@ -166,7 +166,7 @@ const extractConcernsFromDisasters = async (llmModel: LLMModel) => {
   }
   const concerns: Concern[] = [];
   // const llm = new ChatOpenAI({ temperature: 0 });
-  const { modelName, concernDirName } = llmModel;
+  const { modelName, modelDirName } = llmModel;
   const llm = new ChatOllama({
     model: modelName,
   });
@@ -199,6 +199,8 @@ const extractConcernsFromDisasters = async (llmModel: LLMModel) => {
       if (disasterDateChanged < dateOneWeekAgo) {
         continue;
       }
+      // disasterData.idの文字列を取得
+      const disasterId = disasterData.id;
       // disasterData.fields.nameの文字列を取得
       const disasterName = disasterData.fields.name;
       // disasterData.fields.primary_country.nameの文字列を取得
@@ -208,14 +210,18 @@ const extractConcernsFromDisasters = async (llmModel: LLMModel) => {
       const disasterTypeName = disasterData.fields.primary_type.name;
       // disasterData.fields.descriptionの文字列を取得
       const disasterDescription = disasterData.fields.description;
-      // disasterNameとdisasterPrimaryCountryNameとdisasterTypeNameを改行区切りで出力
+      // disasterIdとdisasterNameとdisasterPrimaryCountryNameとdisasterTypeNameを改行区切りで出力
       console.log("===== ===== ===== =====");
       console.log("===== ===== ===== =====");
       console.log(disasterJsonPath);
       console.log(
-        `${disasterName}\n${disasterPrimaryCountryName}\n${disasterTypeName}`
+        `${disasterId}\n${disasterName}\n${disasterPrimaryCountryName}\n${disasterTypeName}`
       );
-      const disasterSummaryPath = `./tmp/api.reliefweb.int/v1/disasters/${disasterData.id}_${llmModel.modelName}_summary_v0.0.1.txt`;
+      const disasterBaseDir = `./tmp/api.reliefweb.int/v1/disasters/${disasterId.slice(
+        0,
+        1
+      )}/${disasterId.slice(0, 2)}`;
+      const disasterSummaryPath = `${disasterBaseDir}/${llmModel.modelDirName}/summary_v0.0.1.txt`;
       let listedSummarizationResultText = "";
       // check disasterSummaryPath is already exists
       try {
@@ -244,7 +250,7 @@ const extractConcernsFromDisasters = async (llmModel: LLMModel) => {
       console.log("----- ----- ----- ----- -----");
       try {
         // disasterDescriptionをareaWithConcernExtractorChainに入力して結果を出力
-        const areaWithConcernPath = `./tmp/api.reliefweb.int/v1/disasters/${disasterData.id}_${llmModel.modelName}_area_with_concern_v0.0.1.txt`;
+        const areaWithConcernPath = `${disasterBaseDir}/${llmModel.modelDirName}/area_with_concern_v0.0.1.txt`;
         let areaWithConcernResultText = "";
         try {
           const alreadyExists = (await fs.lstat(areaWithConcernPath)).isFile();
@@ -287,7 +293,7 @@ const extractConcernsFromDisasters = async (llmModel: LLMModel) => {
 
   console.log(concerns.length);
 
-  const concernsLatestJsonFilePath = `public/data/api.reliefweb.int/${concernDirName}/latest_concerns.json`;
+  const concernsLatestJsonFilePath = `public/data/api.reliefweb.int/concerns/${modelDirName}/latest_concerns.json`;
 
   try {
     await fs.writeFile(
