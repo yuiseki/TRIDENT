50838cc44e82f0796360b92aaad0c9448674484b
yuiseki <yuiseki@gmail.com>
2024-11-09 14:17:43 +0900

    refactor: rename dialogueEndRef to targetEndRef in useScrollToBottom hook

--- src/hooks/scrollToBottom.ts
+++ src/hooks/scrollToBottom.ts
@@ -4,14 +4,14 @@ import { sleep } from "@/utils/sleep";
 import { useCallback, RefObject } from "react";
 
 export const useScrollToBottom = (
-  dialogueEndRef: RefObject<HTMLDivElement>
+  targetEndRef: RefObject<HTMLDivElement>
 ) => {
   const scrollToBottom = useCallback(async () => {
     await sleep(50);
-    if (dialogueEndRef.current) {
-      dialogueEndRef.current.scrollIntoView({ behavior: "instant" });
+    if (targetEndRef.current) {
+      targetEndRef.current.scrollIntoView({ behavior: "instant" });
     }
-  }, [dialogueEndRef]);
+  }, [targetEndRef]);
 
   return scrollToBottom;
 };
