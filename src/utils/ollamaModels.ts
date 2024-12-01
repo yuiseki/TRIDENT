9a4b16c13568df204b7f8c82379f1e97b2b42ac3
yuiseki <yuiseki@gmail.com>
2024-12-01 17:17:03 +0900

    Update src/utils/ollamaModels.ts

--- src/utils/ollamaModels.ts
+++ src/utils/ollamaModels.ts
@@ -2,52 +2,40 @@ import { LLMModel } from "@/types/LLMModel";
 
 export const ollamaModels: LLMModel[] = [
   {
-    modelName: "qwen2.5:0.5b",
-    modelDirName: "qwen2.5/0.5b",
+    modelName: "gemma2:2b",
+    modelDirName: "gemma2/2b",
   },
   {
-    modelName: "qwen2.5:1.5b",
-    modelDirName: "qwen2.5/1.5b",
+    modelName: "gemma2:9b",
+    modelDirName: "gemma2/9b",
   },
   {
-    modelName: "qwen2:0.5b",
-    modelDirName: "qwen2/0.5b",
+    modelName: "granite3-dense:2b",
+    modelDirName: "granite3-dense/2b",
   },
   {
-    modelName: "qwen2:1.5b",
-    modelDirName: "qwen2/1.5b",
+    modelName: "granite3-dense:8b",
+    modelDirName: "granite3-dense/8b",
   },
   {
-    modelName: "qwen:0.5b",
-    modelDirName: "qwen/0.5b",
+    modelName: "granite3-moe:1b",
+    modelDirName: "granite3-moe/1b",
   },
   {
-    modelName: "qwen:1.8b",
-    modelDirName: "qwen/1.8b",
+    modelName: "granite3-moe:3b",
+    modelDirName: "granite3-moe/3b",
   },
   {
     modelName: "llama3.2:1b",
     modelDirName: "llama3.2/1b",
   },
   {
-    modelName: "gemma2:2b",
-    modelDirName: "gemma2/2b",
+    modelName: "llama3.2:3b",
+    modelDirName: "llama3.2/3b",
   },
   {
-    modelName: "gemma:2b-instruct",
-    modelDirName: "gemma/2b-instruct",
-  },
-  {
-    modelName: "codegemma:2b",
-    modelDirName: "codegemma/2b",
-  },
-  {
-    modelName: "orca-mini:3b",
-    modelDirName: "orca-mini/3b",
-  },
-  {
-    modelName: "phi:2.7b",
-    modelDirName: "phi/2.7b",
+    modelName: "qwen2.5:0.5b",
+    modelDirName: "qwen2.5/0.5b",
   },
   {
     modelName: "phi3.5:3.8b",
@@ -58,24 +46,12 @@ export const ollamaModels: LLMModel[] = [
     modelDirName: "phi3/3.8b",
   },
   {
-    modelName: "smollm:135m",
-    modelDirName: "smollm/135m",
-  },
-  {
-    modelName: "smollm:360m",
-    modelDirName: "smollm/360m",
+    modelName: "qwen2.5:1.5b",
+    modelDirName: "qwen2.5/1.5b",
   },
   {
     modelName: "smollm:1.7b",
     modelDirName: "smollm/1.7b",
-  },
-  {
-    modelName: "smollm2:135m",
-    modelDirName: "smollm2/135m",
-  },
-  {
-    modelName: "smollm2:360m",
-    modelDirName: "smollm2/360m",
   },
   {
     modelName: "smollm2:1.7b",
