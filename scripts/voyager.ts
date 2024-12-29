c04df8eeb6396b2b1b1c963efe1e49e513ed1f67
yuiseki <yuiseki@gmail.com>
2024-12-29 10:46:16 +0900

    WIP

--- scripts/voyager.ts
+++ scripts/voyager.ts
@@ -1,5 +1,5 @@
 import { ChatOllama } from "@langchain/ollama";
-import { HumanMessage } from "@langchain/core/messages";
+import { AIMessage, BaseMessage, HumanMessage } from "@langchain/core/messages";
 
 // Tools
 import { Tool } from "langchain/tools";
@@ -7,32 +7,77 @@ import { Wikipedia } from "../src/utils/langchain/tools/wikipedia/index.ts";
 import { Calculator } from "@langchain/community/tools/calculator";
 
 // langgraph
-import { MemorySaver } from "@langchain/langgraph";
+import {
+  MemorySaver,
+  Annotation,
+  messagesStateReducer,
+} from "@langchain/langgraph";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
-
-const model = new ChatOllama({
-  model: "qwen2.5-coder:1.5b",
-  temperature: 0.0,
-});
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
 
 const tools: Array<Tool> = [new Calculator(), new Wikipedia()];
 
-// Initialize memory to persist state between graph runs
-const agentCheckPointer = new MemorySaver();
-const agent = createReactAgent({
-  llm: model,
-  tools: tools,
-  checkpointSaver: agentCheckPointer,
+const toolNode = new ToolNode(tools);
+
+const model = new ChatOllama({
+  model: "qwen2.5-coder:7b",
+  format: "json",
+  temperature: 0,
+}).bindTools(tools);
+
+// Define the graph state
+// See here for more info: https://langchain-ai.github.io/langgraphjs/how-tos/define-state/
+const StateAnnotation = Annotation.Root({
+  messages: Annotation<BaseMessage[]>({
+    // `messagesStateReducer` function defines how `messages` state key should be updated
+    // (in this case it appends new messages to the list and overwrites messages with the same ID)
+    reducer: messagesStateReducer,
+  }),
 });
 
-// Now it's time to use!
-const agentFinalState = await agent.invoke(
-  { messages: [new HumanMessage("Who is the UN Secretary General?")] },
-  { configurable: { thread_id: "42" } }
-);
+// Define the function that determines whether to continue or not
+// We can extract the state typing via `StateAnnotation.State`
+function shouldContinue(state: typeof StateAnnotation.State) {
+  const messages = state.messages;
+  const lastMessage = messages[messages.length - 1] as AIMessage;
 
-console.log(agentFinalState.messages.length);
+  // If the LLM makes a tool call, then we route to the "tools" node
+  if (lastMessage.tool_calls?.length) {
+    return "tools";
+  }
+  // Otherwise, we stop (reply to the user)
+  return "__end__";
+}
 
-console.log(
-  agentFinalState.messages[agentFinalState.messages.length - 1].content
+// Define the function that calls the model
+async function callModel(state: typeof StateAnnotation.State) {
+  const messages = state.messages;
+  const response = await model.invoke(messages);
+
+  // We return a list, because this will get added to the existing list
+  return { messages: [response] };
+}
+
+// Define a new graph
+const workflow = new StateGraph(StateAnnotation)
+  .addNode("agent", callModel)
+  .addNode("tools", toolNode)
+  .addEdge("__start__", "agent")
+  .addConditionalEdges("agent", shouldContinue)
+  .addEdge("tools", "agent");
+
+// Initialize memory to persist state between graph runs
+const checkpointer = new MemorySaver();
+
+// Finally, we compile it into a LangChain Runnable.
+const app = workflow.compile({ checkpointer });
+
+// Use the agent
+const finalState = await app.invoke(
+  {
+    messages: [new HumanMessage("Who is the president of the United States?")],
+  },
+  { configurable: { thread_id: "42" }, recursionLimit: 30 }
 );
+console.log(finalState.messages[finalState.messages.length - 1].content);
--- src/utils/langchain/tools/wikipedia/index.ts
+++ src/utils/langchain/tools/wikipedia/index.ts
@@ -2,25 +2,50 @@ import { Tool } from "langchain/tools";
 
 export class Wikipedia extends Tool {
   name = "search-wikipedia";
-  description = `useful for when you need to ask general questions about people, places, companies, facts, historical events, or other subjects. Input: a search query.`;
+  description = `useful for when you need to ask general questions about people, places, companies, facts, historical events, or other subjects. Input: a very simple search query for wikipedia. MUST NOT input the question as-is.`;
 
   async _call(input: string) {
+    // console.debug("Tool wikipedia, input:", input);
     try {
       const endpoint = "https://en.wikipedia.org/w/api.php";
-      const params = new URLSearchParams();
-      params.append("action", "query");
-      params.append("list", "search");
-      params.append("format", "json");
-      params.append("srsearch", input);
-      const res = await fetch(`${endpoint}?${params.toString()}`);
+      const searchParams = new URLSearchParams();
+      searchParams.append("format", "json");
+      searchParams.append("action", "query");
+      searchParams.append("list", "search");
+      searchParams.append("srsearch", input);
+
+      const res = await fetch(`${endpoint}?${searchParams.toString()}`);
       const json = await res.json();
+
       let answer = json["query"]["search"][0]["snippet"];
       answer = answer.replaceAll(/<[^>]*>/g, "");
       answer = answer.replaceAll("&quot;", "**");
+
+      if ("title" in json["query"]["search"][0]) {
+        const title = json["query"]["search"][0]["title"];
+        const summaryParams = new URLSearchParams();
+        summaryParams.append("format", "json");
+        summaryParams.append("action", "query");
+        summaryParams.append("prop", "extracts");
+        summaryParams.append("exintro", "");
+        summaryParams.append("explaintext", "");
+        summaryParams.append("titles", title);
+        const summaryRes = await fetch(
+          `${endpoint}?${summaryParams.toString()}`
+        );
+        const summaryJson = await summaryRes.json();
+        const pageId = Object.keys(summaryJson["query"]["pages"])[0];
+        const extract = summaryJson["query"]["pages"][pageId]["extract"];
+        answer = extract;
+        answer = `# ${json["query"]["search"][0]["title"]}:\n${answer}`;
+      }
       if ("pageid" in json["query"]["search"][0]) {
         let pageId = json["query"]["search"][0]["pageid"];
         answer = `${answer}\nurl: https://en.wikipedia.org/?curid=${pageId}`;
       }
+      // console.debug("Tool wikipedia, answer:");
+      // console.debug(answer);
+      // console.debug("");
       return answer;
     } catch (error) {
       return "I don't know.";
