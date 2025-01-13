c3ca4a0bfd7f7509fdb7babb6fbcc23f24ba5875
yuiseki <yuiseki@gmail.com>
2025-01-13 13:55:13 +0900

    WIP

--- src/utils/trident/getChatModel.ts
+++ src/utils/trident/getChatModel.ts
@@ -3,17 +3,17 @@ import { ChatOpenAI } from "@langchain/openai";
 
 export const getChatModel = () => {
   if (process.env.USE_OLLAMA === "1") {
-    if (process.env.OLLAMA_CHAT_MODEL !== undefined) {
-      return new ChatOllama({
-        model: process.env.OLLAMA_CHAT_MODEL,
-        temperature: 0,
-      });
-    } else {
-      return new ChatOllama({
-        model: "qwen2.5:1.5b",
-        temperature: 0,
-      });
-    }
+    return new ChatOllama({
+      model:
+        process.env.OLLAMA_CHAT_MODEL !== undefined
+          ? process.env.OLLAMA_CHAT_MODEL
+          : "qwen2.5:1.5b",
+      baseUrl:
+        process.env.OLLAMA_BASE_URL !== undefined
+          ? process.env.OLLAMA_BASE_URL
+          : "http://127.0.0.1:11434",
+      temperature: 0,
+    });
   } else if (process.env.CLOUDFLARE_AI_GATEWAY) {
     return new ChatOpenAI({
       configuration: {
--- src/utils/trident/getEmbeddingModel.ts
+++ src/utils/trident/getEmbeddingModel.ts
@@ -3,15 +3,16 @@ import { OpenAIEmbeddings } from "@langchain/openai";
 
 export const getEmbeddingModel = () => {
   if (process.env.USE_OLLAMA === "1") {
-    if (process.env.OLLAMA_EMBEDDING_MODEL !== undefined) {
-      return new OllamaEmbeddings({
-        model: process.env.OLLAMA_EMBEDDING_MODEL,
-      });
-    } else {
-      return new OllamaEmbeddings({
-        model: "snowflake-arctic-embed:22m",
-      });
-    }
+    return new OllamaEmbeddings({
+      model:
+        process.env.OLLAMA_EMBEDDING_MODEL !== undefined
+          ? process.env.OLLAMA_EMBEDDING_MODEL
+          : "snowflake-arctic-embed:22m",
+      baseUrl:
+        process.env.OLLAMA_BASE_URL !== undefined
+          ? process.env.OLLAMA_BASE_URL
+          : "http://127.0.0.1:11434",
+    });
   } else if (process.env.CLOUDFLARE_AI_GATEWAY) {
     return new OpenAIEmbeddings({
       configuration: {
