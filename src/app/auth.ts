38346b1d175e7c254a6b4e4ecb9e66d5ff36656f
yuiseki <yuiseki@gmail.com>
2025-02-22 11:26:49 +0900

    Setup Prisma adapter

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,20 +1,13 @@
 import NextAuth from "next-auth";
+
+// Providers
 import Twitter from "next-auth/providers/twitter";
-import type { Provider } from "next-auth/providers";
 
-const providers: Provider[] = [Twitter];
-
-export const providerMap = providers
-  .map((provider) => {
-    if (typeof provider === "function") {
-      const providerData = provider();
-      return { id: providerData.id, name: providerData.name };
-    } else {
-      return { id: provider.id, name: provider.name };
-    }
-  })
-  .filter((provider) => provider.id !== "credentials");
+// Adapters
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/app/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Twitter],
+  adapter: PrismaAdapter(prisma),
 });
