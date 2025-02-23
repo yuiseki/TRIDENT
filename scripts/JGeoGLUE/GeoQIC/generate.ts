d5c25bcc4898b6b7244b17b185f29fdd16789ee0
yuiseki <yuiseki@gmail.com>
2025-02-23 14:06:33 +0900

    WIP

--- scripts/JGeoGLUE/GeoQIC/generate.ts
+++ scripts/JGeoGLUE/GeoQIC/generate.ts
@@ -1,10 +1,29 @@
+// label: 「地名検索」、「経路検索」、「施設情報検索」、「レビュー検索」、「その他」
+// name_search, route, facility, review, others
+
 const examples = [
   {
     sentence: "新宿駅から渋谷駅まで",
     label: "route",
   },
   {
+    sentence: "東京タワーに行きたい",
+    label: "route",
+  },
+  {
     sentence: "大阪城の入場料は？",
     label: "facility",
+  },
+  {
+    sentence: "東京スカイツリーの営業時間は？ ",
+    label: "facility",
+  },
+  {
+    sentence: "台東区の家系ラーメンを教えて",
+    label: "review",
+  },
+  {
+    sentence: "渋谷周辺のオススメのレストランを教えて",
+    label: "review",
   },
 ];
