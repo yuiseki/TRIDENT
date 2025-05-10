48f2169d5bface3e9cb18b3d00d9c75ddc0cb026
yuiseki <yuiseki@gmail.com>
2025-05-10 09:31:21 +0900

    fix type

--- src/hooks/scrollToBottom.ts
+++ src/hooks/scrollToBottom.ts
@@ -3,7 +3,9 @@
 import { sleep } from "@/lib/sleep";
 import { useCallback, RefObject } from "react";
 
-export const useScrollToBottom = (targetEndRef: RefObject<HTMLDivElement>) => {
+export const useScrollToBottom = (
+  targetEndRef: RefObject<HTMLDivElement | null>
+) => {
   const scrollToBottom = useCallback(async () => {
     await sleep(50);
     if (targetEndRef.current) {
